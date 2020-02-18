93b19a8c6bffc4972a45413fb6e30f6e4475a942
fionchadd <hello@cogdiss.com>
2020-02-18 15:36:29 +0000

    Update front-page.php

--- front-page.php
+++ front-page.php
@@ -10,12 +10,15 @@
 
 get_header();
 ?>
-
+<?php 
+$blog_title = get_bloginfo( 'name' ); 
+$blog_tagline = get_bloginfo( 'description' ); 
+?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
 
-		<h1 class="page-title screen-reader-text"><?php printf( esc_html__( 'textdomain' ), get_bloginfo ( 'name' ) ); ?></h1>
-		<h2 class="page-subtitle screen-reader-text"><?php printf( esc_html__( 'textdomain' ), get_bloginfo ( 'description' ) ); ?></h2>
+		<h1 class="page-title screen-reader-text"><?php echo $$blog_title ?></h1>
+		<h2 class="page-subtitle screen-reader-text"><?php echo $$blog_tagline ?></h2>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
