b950c8ae177c783e752ad21ee6e026ef429b753c
fionchadd <hello@cogdiss.com>
2020-02-18 14:32:05 +0000

    w00t, it works!

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
  * The header for our theme
  *
  * This is the template that displays all of the <head> section and everything up until <div id="content">
- * testing deployment
+ * 
  *
  * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
  *
