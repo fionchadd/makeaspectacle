77be8025ed2a6104095b2d46ea22ad6c3a136bdb
fionchadd <hello@cogdiss.com>
2020-02-19 13:48:48 +0000

    Adding project section to homepage

--- front-page.php
+++ front-page.php
@@ -13,6 +13,7 @@ get_header();
 <?php 
 $blog_title = get_bloginfo( 'name' ); 
 $blog_tagline = get_bloginfo( 'description' ); 
+$page_tagline = get_field( 'homepage_tagline');
 ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
@@ -30,7 +31,33 @@ $blog_tagline = get_bloginfo( 'description' );
 		<h1 class="page-title"><?php echo $blog_title; ?></h1>
 		<h2 class="page-subtitle"><?php echo $blog_tagline; ?></h2>
 </div>
-		<p class="page-offering">We specialise in creating websites for people in the creative industries.</p>
+		<p class="page-offering"><?php echo $page_tagline; ?></p>
+</div>
+
+<div class="projects">
+<?php if( have_rows('projects') ): 
+		while( have_rows('projects') ): the_row(); 
+		if ( get_field( 'project_details' ) ) : 
+			$details = get_sub_field('project_details'); 
+				$name = $details['text'];
+				$link = $details['url'];
+				$target = $details['target'];
+		endif;	
+		$image = get_sub_field('image'); 
+		if( $image ):
+
+			// Image variables.
+			$url = $image['url'];
+			$imagetitle = $image['title'];
+			$alt = $image['alt'];
+		
+			// Thumbnail size attributes.
+			$size = 'large';
+			$fullimage = $image['sizes'][ $size ];
+		endif;
+?>
+<div class="project"><h4 class="project-name"><a target="<?php echo $target; ?>" href="<?php echo $link; ?>"><?php echo $name; ?></a></h4><img class="project-image" src="<?php echo esc_url($fullimage); ?>" alt="<?php echo esc_attr($alt); ?>" /></div>
+<?php endwhile; else : endif; ?>
 </div>
 		</main><!-- #main -->
 	</div><!-- #primary -->
