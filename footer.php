ee2821484e0da13c332896b11f6f4531c83e0052
fionchadd <hello@cogdiss.com>
2020-02-27 14:54:04 +0000

    Update footer.php

--- footer.php
+++ footer.php
@@ -31,7 +31,7 @@
 				<div class="lid"><div class="lid-inner"><img src="/wp-content/themes/makeaspectacle/sass/images/logo-eyelid-white.svg"></div></div>
 			</div>
 			<div class="eye-background">
-				<img src="/wp-content/themes/makeaspectacle/sass/images/logo-background-dark.svg">
+				<img src="/wp-content/themes/makeaspectacle/sass/images/logo-background-white-01.svg">
 			</div>
 		</div>
 			<h3 class="about-title"><?php echo $blog_title; ?></h3>
