6c69afd9e1ede36ff31f96d4a2f8d844cf274b36
fionchadd <hello@cogdiss.com>
2020-02-28 14:51:12 +0000

    Fixing eye at top of page

--- js/makeaspectaclescripts.js
+++ js/makeaspectaclescripts.js
@@ -244,27 +244,16 @@ jQuery(document).ready(function () {
 
     var controller = new ScrollMagic.Controller({
 
-        globalSceneOptions: {
-          triggerHook: 'onLeave',
-          duration: "0" // this works just fine with duration 0 as well
-          // However with large numbers (>20) of pinned sections display errors can occur so every section should be unpinned once it's covered by the next section.
-          // Normally 100% would work for this, but here 200% is used, as Panel 3 is shown for more than 100% of scrollheight due to the pause.
-        }
       });
       // get all slides
 var slide = document.querySelectorAll("#stickThis");
 var width = window.innerWidth;
 var height = window.innerHeight;
-new ScrollMagic.Scene({
-    triggerElement: slide,
-    offset: 0,
-  })
-  .setPin(slide, {pushFollowers: false})
-  .addTo(controller);
 
   new ScrollMagic.Scene({
 	triggerElement: "#stickThis", 
-	triggerHook: "onLeave",
+    triggerHook: 40,
+    reverse: true,
 })
 						.setPin(".homepage-eye-wrapper")
 //						.addIndicators({name: "1 (duration: 360)"}) // add indicators (requires plugin)
