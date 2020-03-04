d78bbc8365170f18e34ae31f810f317ca9c929ee
fionchadd <hello@cogdiss.com>
2020-03-04 15:33:59 +0000

    Adding 404 page

--- 404.php
+++ 404.php
@@ -12,14 +12,21 @@ get_header();
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
-
+		<div class="eye-wrapper error-eye-wrapper">
+			<div class="eye error-eye" id="eye4">
+				<div class="pupil" id="pupil4"></div>
+			</div>
+			<div class="eye-background">
+				<img src="/wp-content/themes/makeaspectacle/sass/images/logo-outline-dark-w-background.svg">
+			</div>
+		</div>
 			<section class="error-404 not-found">
 				<header class="page-header">
-					<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'makeaspectacle' ); ?></h1>
+					<h1 class="page-title">Oh no!</h1>
 				</header><!-- .page-header -->
 
 				<div class="page-content">
-					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'makeaspectacle' ); ?></p>
+					<p>We couldn't find that page. Maybe try a search? Or take a look at our work <a href="http://new.makeaspectacle.co.uk/#work">here</a></p>
 
 					<?php
 					get_search_form();
@@ -27,28 +34,6 @@ get_header();
 					the_widget( 'WP_Widget_Recent_Posts' );
 					?>
 
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'makeaspectacle' ); ?></h2>
-						<ul>
-							<?php
-							wp_list_categories( array(
-								'orderby'    => 'count',
-								'order'      => 'DESC',
-								'show_count' => 1,
-								'title_li'   => '',
-								'number'     => 10,
-							) );
-							?>
-						</ul>
-					</div><!-- .widget -->
-
-					<?php
-					/* translators: %1$s: smiley */
-					$makeaspectacle_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'makeaspectacle' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$makeaspectacle_archive_content" );
-
-					the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
 
 				</div><!-- .page-content -->
 			</section><!-- .error-404 -->
