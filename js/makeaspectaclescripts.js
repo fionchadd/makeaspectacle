b13c6104540337ded55df5adaa9e637d6476afc9
fionchadd <hello@cogdiss.com>
2020-03-04 12:53:10 +0000

    redrawing about eye when about section open?

--- js/makeaspectaclescripts.js
+++ js/makeaspectaclescripts.js
@@ -46,6 +46,7 @@ var eye2 = new DrawEye("#eye2", "#pupil2", 4, 30);
   
         jQuery('.about-section').toggleClass('open');
         jQuery('body').toggleClass('aboutisopen');
+        var eye2 = new DrawEye("#eye2", "#pupil2", 4, 30);
       });
       jQuery('.about-section .close').on('click', function () {
   
