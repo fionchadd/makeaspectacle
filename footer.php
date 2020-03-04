492365f8bf4f83eaec1c560281200cdfe8c505c0
fionchadd <hello@cogdiss.com>
2020-03-04 14:13:08 +0000

    Update footer.php

--- footer.php
+++ footer.php
@@ -64,8 +64,9 @@
 <?php wp_footer(); ?>
 <script src="/wp-content/themes/makeaspectacle/js/jquery.waypoints.min.js"></script> <!--  waypoints --> 
  <script src="/wp-content/themes/makeaspectacle/js/inview.min.js"></script>  <!-- inview --> 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.2.4/gsap.min.js"></script> <!-- GSAP for scrollmagic --> 
-<script src="//cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.7/ScrollMagic.js"></script> <!-- scrollmagic for scrolling effects --> 
-<script src="//cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.7/plugins/debug.addIndicators.min.js"></script> <!-- indicators for scrollmagic --> 
+ <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/2.1.3/TweenMax.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/2.1.3/TweenLite.min.js"></script>
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.7/ScrollMagic.min.js"></script>
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.7/plugins/animation.gsap.js"></script>
 </body>
 </html>
