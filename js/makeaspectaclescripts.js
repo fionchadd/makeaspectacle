1d8a24ff88ecdf7aee3892bd40c8615c1ddc82be
fionchadd <hello@cogdiss.com>
2020-03-05 15:02:31 +0000

    Changing trigger hook again

--- js/makeaspectaclescripts.js
+++ js/makeaspectaclescripts.js
@@ -159,7 +159,7 @@ $('.project').each(function(){
     // Create a scene for each project
     var myScene = new ScrollMagic.Scene({
     triggerElement: this,
-    triggerHook: 0.8
+    triggerHook: 0.6
     })
     .setClassToggle(this, 'active')
     .addTo(controller);
