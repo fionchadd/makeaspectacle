4b1aab8715ed9fefc05ae15d748778ff236bd669
fionchadd <hello@cogdiss.com>
2020-03-05 12:57:14 +0000

    camelcase

--- js/makeaspectaclescripts.js
+++ js/makeaspectaclescripts.js
@@ -101,7 +101,7 @@ var eye3 = new DrawEye("#eye3", "#pupil3", 4, 30);
 var tl = new TimelineMax();
 tl.fromTo(".above-fold .page-title", {opacity: 1}, {duration: 1, opacity: 0});
 tl.fromTo(".above-fold .page-subtitle", {opacity: 1}, {duration: 1, opacity: 0});
-tl.fromTo(".above-fold .page-offering", {opacity: 1, transform: "translatey(0)"}, {duration: 1, opacity: 0, transform: "translatey(-100px)"});
+tl.fromTo(".above-fold .page-offering", {opacity: 1, transform: "translateY(0)"}, {duration: 1, opacity: 0, transform: "translateY(-100px)"});
 
 
 
