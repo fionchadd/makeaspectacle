f527db329a253737e7fd67e497188a75fe9d5235
fionchadd <hello@cogdiss.com>
2020-03-05 14:46:10 +0000

    Trying to add active class individually

--- js/makeaspectaclescripts.js
+++ js/makeaspectaclescripts.js
@@ -163,6 +163,23 @@ new ScrollMagic.Scene({
 .setClassToggle(this, "active") // add class toggle
 .addTo(controller);
 
+var $element = $('.project');
+
+
+scene = new ScrollMagic.Scene({
+        triggerElement: $element[0],
+        triggerHook: "onEnter",
+        offset: 350,
+      })
+      .addTo(controller);
+
+    scene.on('enter', function(){
+      $element.addClass('active-class');
+    });
+
+    scene.on('leave', function(){
+      $element.removeClass('active-class');
+    });
 
 });  
 /*
