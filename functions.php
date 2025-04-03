562b82b31b357c384b1e7a28f746b6cf26045ec0
fionchadd <hello@cogdiss.com>
2025-04-03 12:14:20 +0100

    changing name of js file

--- functions.php
+++ functions.php
@@ -129,7 +129,7 @@ function makeaspectacle_scripts() {
 	wp_enqueue_style( 'makeaspectacle-style', get_stylesheet_uri(), array(), _S_VERSION );
 
 	wp_enqueue_script( 'makeaspectacle-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
-	wp_enqueue_script( 'makeaspectacle-scripts', get_template_directory_uri() . '/js/makeaspectaclescripts.js', array('jquery'), _S_VERSION, true );
+	wp_enqueue_script( 'makeaspectacle-scripts', get_template_directory_uri() . '/js/scripts.js', array('jquery'), _S_VERSION, true );
 
 
 	wp_enqueue_script( 'makeaspectacle-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), _S_VERSION, true );
