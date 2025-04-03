59a6d4e719cf54c0936b80d357be8cb0c6ec4708
fionchadd <hello@cogdiss.com>
2025-04-03 11:50:26 +0100

    enqueueing stylesheet form the correct place

--- functions.php
+++ functions.php
@@ -7,6 +7,12 @@
  * @package Make_A_Spectacle
  */
 
+
+ if ( ! defined( '_S_VERSION' ) ) {
+	// Replace the version number of the theme on each release.
+	define( '_S_VERSION', '1.0.0' );
+}
+
 if ( ! function_exists( 'makeaspectacle_setup' ) ) :
 	/**
 	 * Sets up theme defaults and registers support for various WordPress features.
@@ -120,13 +126,13 @@ add_action( 'widgets_init', 'makeaspectacle_widgets_init' );
  * Enqueue scripts and styles.
  */
 function makeaspectacle_scripts() {
-	wp_enqueue_style( 'makeaspectacle-style', get_template_directory_uri() . '/sass/style.css');
+	wp_enqueue_style( 'makeaspectacle-style', get_stylesheet_uri(), array(), _S_VERSION );
 
-	wp_enqueue_script( 'makeaspectacle-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
-	wp_enqueue_script( 'makeaspectacle-scripts', get_template_directory_uri() . '/js/makeaspectaclescripts.js', array('jquery'), true );
+	wp_enqueue_script( 'makeaspectacle-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
+	wp_enqueue_script( 'makeaspectacle-scripts', get_template_directory_uri() . '/js/makeaspectaclescripts.js', array('jquery'), _S_VERSION, true );
 
 
-	wp_enqueue_script( 'makeaspectacle-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+	wp_enqueue_script( 'makeaspectacle-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), _S_VERSION, true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
